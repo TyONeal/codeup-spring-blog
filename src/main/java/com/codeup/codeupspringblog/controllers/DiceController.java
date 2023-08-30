c9cf3a96492cef9bbbac32693bd9e76b7a87330e
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-08-30 16:56:28 -0500

    problem 1 bonus done in view-exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/DiceController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/DiceController.java
@@ -14,14 +14,24 @@ public class DiceController {
         return "roll-dice";
     }
 
-    @GetMapping ("/roll-dice/{userGuess}")
-    public String guessResults(@PathVariable int userGuess, Model model) {
-        Random rand = new Random();
-        int randomChoice = rand.nextInt(5) + 1;
-        model.addAttribute("userGuess", userGuess);
-        model.addAttribute("randomNumber", randomChoice);
+    @GetMapping ("/roll-dice/guess")
+    public String guessResults() {
+
         return "dice-results";
     }
 
+    @PostMapping("/roll-dice/guess")
+    public String showGuessResults(@RequestParam(name = "userGuess") String guess, Model model) {
+        System.out.println(guess);
+        Random rand = new Random();
+        int diceRoll1 = rand.nextInt(5) + 1;
+        int diceRoll2 = rand.nextInt(5) + 1;
+        int diceRoll3 = rand.nextInt(5) + 1;
+        int diceRoll4 = rand.nextInt(5) + 1;
+        int diceTotal = diceRoll1 + diceRoll2 + diceRoll3 + diceRoll4;
+        model.addAttribute("userGuess", guess);
+        model.addAttribute("diceTotal", diceTotal);
 
+        return "dice-results";
+    }
 }
