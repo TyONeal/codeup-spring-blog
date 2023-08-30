c95a129d4fa1ffade205e882b0b34935319484de
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-08-30 11:22:50 -0500

    finished controller exercise

---
+++ src/main/java/controllers/PostController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @ResponseBody
+    public String posts() {
+        return "posts index page";
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String postsById(@PathVariable int id) {
+        return "view an individual post";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String viewCreatePost() {
+        return "view the form for creating a post";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String createPost() {
+        return "create a new post";
+    }
+
+}
