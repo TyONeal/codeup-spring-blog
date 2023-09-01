13af04d858448173a31e8604892c0c931ebe0ff5
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-09-01 10:20:10 -0500

    finished views exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,21 +1,42 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.entities.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String posts() {
-        return "posts index page";
+    public String posts(Model model) {
+        ArrayList<Post> postList = new ArrayList<>();
+
+        Post post1 = new Post();
+        post1.setTitle("test title 1");
+        post1.setBody("test body 1");
+
+        Post post2 = new Post();
+        post2.setTitle("test title 2");
+        post2.setBody("test body 2");
+
+        postList.add(post1);
+        postList.add(post2);
+
+        model.addAttribute("post", postList);
+        return "posts/index";
     }
 
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String postsById(@PathVariable int id) {
-        return "view an individual post";
+    @RequestMapping(path = "/singlepost", method = RequestMethod.GET)
+    public String postsById(Model model) {
+        Post post = new Post();
+        post.setTitle("test title 1");
+        post.setBody("test body 1");
+
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
